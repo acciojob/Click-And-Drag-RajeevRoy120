d56ed5672b9ab153b18649d54be393df514b2136
Rajeev Roy <109815313+RajeevRoy120@users.noreply.github.com>
2025-03-21 09:04:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Your code here.
+var draggable = document.getElementById('draggable');
+
+draggable.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        draggable.offsetLeft - e.clientX,
+        draggable.offsetTop - e.clientY
+    ];
+}, true);
+
+document.addEventListener('mouseup', function() {
+    isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown) {
+        mousePosition = {
+            x : event.clientX,
+            y : event.clientY
+        };
+        draggable.style.left = (mousePosition.x + offset[0]) + 'px';
+        draggable.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+}, true);
